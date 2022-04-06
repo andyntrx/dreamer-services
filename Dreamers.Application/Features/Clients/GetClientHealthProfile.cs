8c136c02f17e1cab9240f3abffef191382a5f14b
Andy Nguyen <andyntrx@hotmail.com>
2022-04-05 21:58:28 -0500

    add health and tax profiles

---
+++ Dreamers.Application/Features/Clients/GetClientHealthProfile.cs
@@ -0,0 +1,35 @@
+﻿using Dreamers.Domain.Entities.Healths;
+using Dreamers.Infra.Data.Context;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dreamers.Application.Features.Clients
+{
+    public class GetClientHealthProfile : IRequest<ClientHealthProfile>
+    {
+        public Guid Ucid { get; set; }
+    }
+
+    public class GetClientHealthProfileHandler : IRequestHandler<GetClientHealthProfile, ClientHealthProfile>
+    {
+        readonly DreamContext _context;
+        public GetClientHealthProfileHandler(DreamContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ClientHealthProfile> Handle(GetClientHealthProfile request, CancellationToken cancellationToken)
+        {
+            var client = await _context.Clients.FindAsync(request.Ucid);
+
+            return new ClientHealthProfile
+            {
+
+            };
+        }
+    }
+}
